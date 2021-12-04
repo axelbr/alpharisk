11ec866e4b48e95d5a9aa18ff09b5b85478e3c7b
Julian <julian.lemmel@gmail.com>
2021-12-04 15:40:17 +0100

    Alpharisk muzero aiDecision, DOESNT COMPILE

--- src/at/ac/tuwien/ifs/sge/agent/alpharisk/AlphaRisk.java
+++ src/at/ac/tuwien/ifs/sge/agent/alpharisk/AlphaRisk.java
@@ -25,37 +25,9 @@ public class AlphaRisk extends AbstractGameAgent<Risk, RiskAction> implements Ga
     public RiskAction computeNextAction(final Risk game, final long computationTime, final TimeUnit timeUnit) {
 
         List<Integer> actions = new ArrayList<>();
-        int
-            nextMoveInt =
+        final int nextMoveInt =
             Inference.aiDecision(actions, true, "./pretrained", RiskConfigFactory.getRiskInstance());
-
-        super.setTimers(computationTime, timeUnit);
-        this.nanosElapsed();
-        this.nanosLeft();
-        this.shouldStopComputation();
-        final RiskBoard board = game.getBoard();
-        board.getNrOfTerritoriesOccupiedByPlayer(this.playerId);
-        game.getHeuristicValue();
-        game.getHeuristicValue(this.playerId);
-        final Set<RiskAction> possibleActions = game.getPossibleActions();
-        double bestUtilityValue = Double.NEGATIVE_INFINITY;
-        double bestHeuristicValue = Double.NEGATIVE_INFINITY;
-        RiskAction bestAction = null;
-        for (final RiskAction possibleAction : possibleActions) {
-            final Risk next = (Risk) game.doAction(possibleAction);
-            final double nextUtilityValue = next.getUtilityValue(this.playerId);
-            final double nextHeuristicValue = next.getHeuristicValue(this.playerId);
-            if (bestUtilityValue <= nextUtilityValue && (bestUtilityValue < nextUtilityValue
-                                                         || bestHeuristicValue <= nextHeuristicValue)) {
-                bestUtilityValue = nextUtilityValue;
-                bestHeuristicValue = nextHeuristicValue;
-                bestAction = possibleAction;
-            }
-        }
-        assert bestAction != null;
-        assert game.isValidAction(bestAction);
-        this.log.debugf("Found best move: %s", bestAction.toString());
-        return bestAction;
+        return game.getPossibleActions()[nextMoveInt];
     }
 
     public void tearDown() {
